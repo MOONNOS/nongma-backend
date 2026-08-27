06847c68634aa4818252b0411e6ee3b43edd99a5
MOONNOS <parkker.moonnos@gmail.com>
2026-08-27 16:18:35 +0700

    Update courses.js

--- routes/courses.js
+++ routes/courses.js
@@ -1,18 +1,21 @@
 const express = require("express");
-const db = require("../db");
-
+const { all } = require("../db");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  const courses = db.prepare("SELECT * FROM courses WHERE status = 1 ORDER BY sort_order ASC").all();
-  const result = courses.map((c) => ({
-    id: c.id,
-    title: c.title,
-    icon: c.icon,
-    description: c.description,
-    lessons: JSON.parse(c.lessons || "[]"),
-  }));
-  res.json(result);
+router.get("/", async (req, res, next) => {
+  try {
+    const courses = await all("SELECT * FROM courses WHERE status = 1 ORDER BY sort_order ASC");
+    const result = courses.map((c) => ({
+      id: c.id,
+      title: c.title,
+      icon: c.icon,
+      description: c.description,
+      lessons: JSON.parse(c.lessons || "[]"),
+    }));
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
