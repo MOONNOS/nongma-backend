ced1e164a12f90f9ef7a904636fa1c3b17576020
MOONNOS <parkker.moonnos@gmail.com>
2026-08-28 14:54:18 +0700

    Update community.js

--- routes/community.js
+++ routes/community.js
@@ -1,27 +1,28 @@
 const express = require("express");
-const db = require("../db");
+const { get, all } = require("../db");
 const { optionalAuth } = require("../middleware/auth");
-
 const router = express.Router();
 
-router.get("/", optionalAuth, (req, res) => {
-  let userLevel = 1;
-  if (req.userId) {
-    const user = db.prepare("SELECT level FROM users WHERE id = ?").get(req.userId);
-    if (user) userLevel = user.level;
+router.get("/", optionalAuth, async (req, res, next) => {
+  try {
+    let userLevel = 1;
+    if (req.userId) {
+      const user = await get("SELECT level FROM users WHERE id = ?", [req.userId]);
+      if (user) userLevel = user.level;
+    }
+    const links = await all("SELECT * FROM community_links ORDER BY sort_order ASC");
+    const result = links.map((l) => ({
+      id: l.id,
+      platform: l.platform,
+      icon: l.icon,
+      level_required: l.level_required,
+      unlocked: userLevel >= l.level_required,
+      url: userLevel >= l.level_required ? l.url : null,
+    }));
+    res.json(result);
+  } catch (err) {
+    next(err);
   }
-
-  const links = db.prepare("SELECT * FROM community_links ORDER BY sort_order ASC").all();
-  const result = links.map((l) => ({
-    id: l.id,
-    platform: l.platform,
-    icon: l.icon,
-    level_required: l.level_required,
-    unlocked: userLevel >= l.level_required,
-    url: userLevel >= l.level_required ? l.url : null,
-  }));
-
-  res.json(result);
 });
 
 module.exports = router;
